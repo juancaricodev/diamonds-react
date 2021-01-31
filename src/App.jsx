ad5921246d135b5b3249fc8edef545977370b305
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-31 02:58:55 -0500

    feat: Header laid out with styles corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 
-import '@styles/components/App.styl'
-import Logo from '@svg/logo.svg'
+import Header from '@components/Header'
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello World! we are here trying out the Poppins font. aAa</h1>
-      <button type='button'>Just a Button</button>
-      <img src={Logo} alt='logo' />
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,86 @@
 import React from 'react'
 
+import '@styles/components/Header.styl'
+import diamondIcon from '@svg/diamond.svg'
+import expandIcon from '@svg/expand-button.svg'
+import lockIcon from '@svg/lock.svg'
+import logoIcon from '@svg/logo.svg'
+import searchIcon from '@svg/magnifying-glass.svg'
+
 const Header = () => {
   return (
-    <>
-      <div>f</div>
-    </>
+    <header className='header'>
+      <div className='header__login'>
+        <img src={lockIcon} alt='login logo' />
+        <p>Login</p>
+      </div>
+
+      <nav className='header__navbar'>
+        <div>
+          <a href='#home'>
+            <p>HOME</p>
+          </a>
+          <img
+            className='header__navbar-diamond'
+            src={diamondIcon}
+            alt='diamond icon'
+          />
+        </div>
+
+        <div>
+          <a href='#about'>
+            <p>ABOUT</p>
+          </a>
+          <img
+            className='header__navbar-diamond'
+            src={diamondIcon}
+            alt='diamond icon'
+          />
+        </div>
+
+        <div>
+          <img
+            className='header__navbar-logo'
+            src={logoIcon}
+            alt='diamond logo'
+          />
+        </div>
+
+        <div>
+          <a href='#jewels'>
+            <p>JEWELS</p>
+          </a>
+          <img
+            className='header__navbar-diamond'
+            src={diamondIcon}
+            alt='diamond icon'
+          />
+        </div>
+
+        <div>
+          <a href='#contact'>
+            <p>CONTACT</p>
+          </a>
+          <img
+            className='header__navbar-diamond'
+            src={diamondIcon}
+            alt='diamond icon'
+          />
+        </div>
+      </nav>
+
+      <div className='header__search'>
+        <div className='header__search-lang'>
+          <p>EN</p>
+          <img src={expandIcon} alt='expand icon' />
+        </div>
+        <img
+          className='header__search-icon'
+          src={searchIcon}
+          alt='search icon'
+        />
+      </div>
+    </header>
   )
 }
 
