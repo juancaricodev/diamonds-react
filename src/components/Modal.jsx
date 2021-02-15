44a2a26f466ee8e9092069d8b974fde682b9cc9c
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-15 16:06:29 -0500

    feat(Modal): template implemented, pending functionality

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,13 +7,64 @@ import '@styles/Modal.styl'
 const Modal = ({ open }) => {
   if (!open) return null
 
-  return (
-    ReactDOM.createPortal(
-      <div className='modal'>
-        Hola, soy um modal
+  return ReactDOM.createPortal(
+    <section className='modal'>
+      <div className='modal-close'>Close</div>
+
+      <div className='modal-container'>
+        <div className='modal-container__header'>
+          <h1>Contact</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+            architecto doloribus.
+          </p>
+        </div>
+
+        <form className='modal-container__form'>
+          <p className='modal-container__form-group'>
+            <label htmlFor=''>This field is required.</label>
+            <input
+              type='text'
+              name='name'
+              placeholder='Name'
+            />
+          </p>
+
+          <p className='modal-container__form-group'>
+            <label>This field is required.</label>
+            <label>This email address is invalid.</label>
+            <input
+              type='text'
+              name='email'
+              placeholder='Email'
+            />
+          </p>
+
+          <p className='modal-container__form-group'>
+            <label htmlFor=''>This field is required.</label>
+            <textarea
+              name='message'
+              cols='30'
+              rows='8'
+              placeholder='Your message here.'
+            />
+          </p>
+
+          <p className='modal-container__form-server-error'>
+            Unexpected server error ...
+          </p>
+        </form>
       </div>
-      , document.body
-    )
+
+      <button
+        type='submit'
+        className='modal__submit'
+      >
+        send
+      </button>
+
+    </section>,
+    document.body
   )
 }
 
