8f831a41e0e7ae6f2f5fcb0ff40c944fbb9cf7f1
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-15 17:10:57 -0500

    feat(Modal): form counting with external button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,6 +7,11 @@ import '@styles/Modal.styl'
 const Modal = ({ open, close }) => {
   if (!open) return null
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.warn('Form Submited')
+  }
+
   return ReactDOM.createPortal(
     <section className='modal'>
       <button type='button' className='modal-close' onClick={close}>
@@ -22,48 +27,53 @@ const Modal = ({ open, close }) => {
           </p>
         </div>
 
-        <form className='modal-container__form'>
-          <p className='modal-container__form-group'>
-            <label htmlFor=''>This field is required.</label>
+        <form className='modal-container__form' onSubmit={handleSubmit}>
+          <div className='modal-container__form-group'>
+            <label htmlFor='name'>This field is required.</label>
             <input
               type='text'
               name='name'
+              id='name'
               placeholder='Name'
             />
-          </p>
+          </div>
 
-          <p className='modal-container__form-group'>
-            <label>This field is required.</label>
-            <label>This email address is invalid.</label>
+          <div className='modal-container__form-group'>
+            <label htmlFor='email'>This field is required.</label>
+            <label htmlFor='email'>This email address is invalid.</label>
             <input
               type='text'
               name='email'
+              id='email'
               placeholder='Email'
             />
-          </p>
+          </div>
 
-          <p className='modal-container__form-group'>
-            <label htmlFor=''>This field is required.</label>
+          <div className='modal-container__form-group'>
+            <label htmlFor='message'>This field is required.</label>
             <textarea
               name='message'
+              id='message'
               cols='30'
               rows='8'
               placeholder='Your message here.'
             />
-          </p>
+          </div>
 
           <p className='modal-container__form-server-error'>
             Unexpected server error ...
           </p>
+
+          <input type='submit' id='submit-form' style={{ visibility: 'hidden' }} />
         </form>
       </div>
 
-      <button
-        type='submit'
-        className='modal__submit'
-      >
+      <label htmlFor='submit-form' className='modal__submit'>
         send
-      </button>
+        {/* <button type='button' className='modal__submit'>
+          send
+        </button> */}
+      </label>
 
     </section>,
     document.body
